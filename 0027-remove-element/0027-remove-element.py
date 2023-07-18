d931d557a85e157dc4deed7b56400e25f41a7247
Adebowale Abiodun Saheed <asaheed88@yahoo.com>
2023-07-18 17:21:50 +0200

    Time: 47 ms (73.05%), Space: 16.3 MB (37.34%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,29 +1,5 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-#         running_idx = 0
-#         available_idx = None
-#         while running_idx < len(nums):
-#             current_el = nums[running_idx]
-            
-#             if current_el != val:
-#                 if available_idx is None:
-#                     running_idx += 1
-#                     continue
-
-#                 nums[available_idx] = current_el
-#                 available_idx += 1
-#                 running_idx += 1
-#                 continue
-
-#             if current_el == val:
-#                 if available_idx is None:
-#                     available_idx = running_idx
-#                 running_idx += 1
-#                 continue
-
-#         return available_idx
-
-
         running_idx = 0
         available_idx = 0
         while running_idx < len(nums):
