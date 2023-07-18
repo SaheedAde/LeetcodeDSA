4aec76a09eddec5b2be95c87efba7f873d10b06b
Adebowale Abiodun Saheed <asaheed88@yahoo.com>
2023-07-18 18:32:38 +0200

    Time: 42 ms (84.99%), Space: 16.4 MB (15.00%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parenth_pairs = {'{': '}', '(': ')', '[': ']'}
+        for p in s:
+            if p in parenth_pairs:
+                stack.append(p)
+                continue
+            
+            if not stack:
+                return False
+            
+            last_open_p = stack.pop()
+            if parenth_pairs[last_open_p] != p:
+                return False
+            
+        if stack:
+            return False
+            
+        return True
+        
