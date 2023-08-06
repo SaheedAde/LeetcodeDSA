b527471f8805a53dc24cc4cb12893f7ff695b813
Adebowale Abiodun Saheed <asaheed88@yahoo.com>
2023-08-06 15:00:06 +0200

    Time: 40 ms (79.76%), Space: 16.5 MB (37.33%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,21 +1,35 @@
 class MyStack:
 
     def __init__(self):
-        self.queue = deque()
+        self.q = deque()
 
     def push(self, x: int) -> None:
-        self.queue.append(x)
+        self.q.append(x)
 
     def pop(self) -> int:
-        return self.queue.pop()
+        # Real Q don't have pop, we can only dequeue from the left.
+        # Using self.queue.pop robs us of the message of this task
+        # return self.queue.pop()
+        q_vals = list(self.q)
+        q_vals.reverse()
+        rev_q = deque(q_vals)
+        popped_val = rev_q.popleft()
+
+        rev_q_vals = list(rev_q)
+        rev_q_vals.reverse()
+        self.q = deque(rev_q_vals)
+        
+        return popped_val
+        
 
     def top(self) -> int:
-        return self.queue[-1]
+        return self.q[-1]
         
     def empty(self) -> bool:
-        return not bool(len(self.queue))
+        return not bool(len(self.q))
         
-
+        
+        
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
