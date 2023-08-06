677dfa5684671bb5bda0e40ba4b48505caef0963
Adebowale Abiodun Saheed <asaheed88@yahoo.com>
2023-08-06 20:16:45 +0200

    Time: 42 ms (89.82%), Space: 22.8 MB (13.74%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,15 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev_node = None
-        current_node = head
-        while current_node:
-            next_node = current_node.next # temp variable
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev_node = None
+#         current_node = head
+#         while current_node:
+#             next_node = current_node.next # temp variable
 
-            current_node.next = prev_node
-            prev_node = current_node
+#             current_node.next = prev_node
+#             prev_node = current_node
             
-            current_node = next_node
+#             current_node = next_node
 
-        return prev_node # Previous ptr is the new head at the end of the loop
+#         return prev_node # Previous ptr is the new head at the end of the loop
+    
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def rec(hd: Optional[ListNode], prev_node: Optional[ListNode] = None) -> Optional[ListNode]:
+            if not hd:
+                return prev_node
+            
+            next_node = hd.next
+            
+            hd.next = prev_node
+            prev_node = hd
+            return rec(next_node, prev_node)
+
+        return rec(head)
+            
